b85b7f218b260a4705a2a51f79944efe68268cda
hepoiko <hepoiko@gmail.com>
2013-07-04 08:23:07 +0000

    Adding PAGE constants, allow user to specific which page to extract from File

---
+++ sanselan-urf/src/main/java/org/apache/sanselan/formats/urf/UrfConstants.java
@@ -0,0 +1,7 @@
+package org.apache.sanselan.formats.urf;
+
+public interface UrfConstants {
+
+	String PAGE = "URF_PAGE";
+	
+}
--- sanselan-urf/src/test/java/org/apache/sanselan/formats/urf/UdfFileReadingTest.java
+++ sanselan-urf/src/test/java/org/apache/sanselan/formats/urf/UdfFileReadingTest.java
@@ -5,11 +5,12 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Collections;
 
 import javax.imageio.ImageIO;
 
 import org.apache.sanselan.ImageParser;
-import org.apache.sanselan.formats.urf.UrfImageParser;
+import org.apache.sanselan.ImageReadException;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -21,15 +22,35 @@ public class UdfFileReadingTest {
 		Dimension dimension = parser.getImageSize(new File(
 				"src/test/samples/sample.urf"));
 		Assert.assertEquals(4960, dimension.width);
-		
+
 		try {
-		    // retrieve image
-		    BufferedImage bi = parser.getBufferedImage(new File(
+			// retrieve image
+			BufferedImage bi = parser.getBufferedImage(new File(
 					"src/test/samples/sample.urf"), null);
-		    File outputfile = new File("target/saved.jpg");
-		    ImageIO.write(bi, "jpg", outputfile);
+			File outputfile = new File("target/saved.jpg");
+			ImageIO.write(bi, "jpg", outputfile);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Test
+	public void testExportPage2() throws ImageReadException, IOException {
+		ImageParser parser = new UrfImageParser();
+		Dimension dimension = parser.getImageSize(new File(
+				"src/test/samples/sample.urf"));
+		Assert.assertEquals(4960, dimension.width);
+
+		try {
+			// retrieve image
+			BufferedImage bi = parser.getBufferedImage(new File(
+					"src/test/samples/sample.urf"), Collections.singletonMap(
+					UrfConstants.PAGE, 2));
+			File outputfile = new File("target/saved2.jpg");
+			ImageIO.write(bi, "jpg", outputfile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
